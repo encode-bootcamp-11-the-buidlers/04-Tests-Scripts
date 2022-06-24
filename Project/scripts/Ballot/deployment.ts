89c927c6bce1e24e726c41ffcd14806a13dd2623
Tobias Leinss <caruso33@web.de>
2022-06-24 21:05:40 +0200

    Implement delegate vote

--- Project/scripts/Ballot/deployment.ts
+++ Project/scripts/Ballot/deployment.ts
@@ -1,11 +1,7 @@
 import "dotenv/config";
 import { ethers } from "ethers";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
-
-// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
-// Do never expose your keys like this
-const EXPOSED_KEY =
-  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+import { getSignerProvider, getWallet } from "./utils";
 
 function convertStringArrayToBytes32(array: string[]) {
   const bytes32Array = [];
@@ -18,21 +14,9 @@ function convertStringArrayToBytes32(array: string[]) {
 async function main() {
   const network = process.argv[2];
 
-  const wallet =
-    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
-      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
-      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
-  console.log(`Using address ${wallet.address}`);
+  const wallet = getWallet();
 
-  let provider:
-    | ethers.providers.JsonRpcProvider
-    | ethers.providers.BaseProvider;
-  if (network === "localhost") {
-    provider = new ethers.providers.JsonRpcProvider();
-  } else {
-    provider = ethers.providers.getDefaultProvider(network);
-  }
-  const signer = wallet.connect(provider);
+  const { signer } = getSignerProvider(wallet, network);
 
   const balanceBN = await signer.getBalance();
   const balance = Number(ethers.utils.formatEther(balanceBN));

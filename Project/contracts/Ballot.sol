79e209efc449eba4da0294f057bd0123290bcecc
Bitcoinera <anagjordano@gmail.com>
2022-07-08 14:08:58 +0300

    some variable packing

--- Project/contracts/Ballot.sol
+++ Project/contracts/Ballot.sol
@@ -7,9 +7,9 @@ contract Ballot {
     // be used for variables later.
     // It will represent a single voter.
     struct Voter {
-        uint256 weight; // weight is accumulated by delegation
         bool voted; // if true, that person already voted
         address delegate; // person delegated to
+        uint256 weight; // weight is accumulated by delegation
         uint256 vote; // index of the voted proposal
     }
 
